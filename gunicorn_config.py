46493a710f33720204338e5e6a5b9f8a07e6dda6
monoluntik <narbkv07@gmail.com>
2023-12-05 16:32:43 +0600

    a

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,10 @@
-import os
-from django.core.wsgi import get_wsgi_application
+workers = 4  # Adjust the number of workers based on your system's capabilities
+worker_class = 'sync'  # You can use 'sync', 'gevent', or 'eventlet' based on your needs
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+bind = '0.0.0.0:8000'  # Binding address and port for Gunicorn
+timeout = 60  # Adjust the timeout as needed
 
-application = get_wsgi_application()
+# Logging configuration
+accesslog = '-'  # Use '-' to log to stdout
+errorlog = '-'   # Use '-' to log to stderr
+loglevel = 'info'  # Adjust the log level based on your needs
