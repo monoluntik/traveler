e6af21f5cd0ca8449dedbb033048126a75f7d583
monoluntik <narbkv07@gmail.com>
2023-12-05 16:26:58 +0600

    a

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,2 +1,10 @@
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+application = get_wsgi_application()
+
+
 workers = 4
 bind = "0.0.0.0:8000"
