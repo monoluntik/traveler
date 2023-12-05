3c49d3639a7c27ef33f514fd55debe3dd1b3a5f3
monoluntik <narbkv07@gmail.com>
2023-12-05 16:40:46 +0600

    s

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django_countries',
     'django_quill',
     'studyAbroad',
